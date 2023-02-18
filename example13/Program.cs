e59aa769db7b42966ac95d0a006e5587f167c7cd
JuliaRomaeva <julia.romaeva@gmail.com>
2023-02-18 19:54:16 +0300

    example13 третья цифра числа

---
+++ example13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число: ");
+string s = Console.ReadLine();
+int l = s.Length;
+if(l > 2) {
+string s1 = Convert.ToString(s[2]);
+Console.Write($" {s} -> {s1}     ");
+}
+else{
+    Console.Write(" третьей цифры нет ");
+}
